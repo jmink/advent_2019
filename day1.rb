49c758c42f2fa31a8e7ef4c3b1d26c3318f7adc5
Jessica Mink <jmink@MacBook-Pro.local>
2019-12-01 14:12:25 -0500

    Day1 solution, passed validation though the code is not production quality

---
+++ day1.rb
@@ -0,0 +1,25 @@
+#!/usr/local/bin/ruby -w
+
+##
+# Simple program to determine the fuel needed for a given mass.
+# This is an extremely rough program with no tests, which could
+# certainly benefit from cleanup.
+##
+
+def fuel_calc(mass)
+  # When dividing ints ruby automatically rounds down
+  (mass.to_i/3) - 2
+end
+
+def total_mass(file_name)
+  # This could be re-written as a fold
+  sum = 0
+  File.open(file_name).each { |mass|
+    fuel = fuel_calc(mass)
+    sum += fuel
+    print "Mass #{mass} will require fuel #{fuel}.  Running total: #{sum}"
+  }
+  sum
+end
+
+total_mass("day1_input.txt")
